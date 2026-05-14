91a111414a8aba09cdc4cdcbf44debc361f9b0ed
Nexeruno <danzby@seznam.cz>
2026-05-14 15:03:21 +0200

    Python web monitoring, Slack alerts and S3 logging

---
+++ README.md.txt
@@ -0,0 +1,7 @@
+Python web monitoring script
+- kontrola dostupnosti webů
+- měření odezvy
+- logování výsledků
+- odesílání upozornění Slacku
+- uložení logu do AWS S3
+- konfigurace přes .env
--- monitoring_system.py
+++ monitoring_system.py
@@ -1,27 +1,28 @@
 import requests
-import smtplib
 import time
-from datetime import datetime
-from email.mime.text import MIMEText
+import os
 import boto3
+from datetime import datetime
+from dotenv import load_dotenv
 
-s3 = boto3.client("s3")
 
-def posli_email(zprava):
-        email = "rezacdaniel2@gmail.com"
-        heslo = "fpeg mksj wdml obgb"
-    
-        msg = MIMEText(zprava)
-        msg ["Subject"] = "Monitoring problém!"
-        msg ["From"] = email
-        msg ["To"] = email
-    
-        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as server:
-            server.login(email, heslo)
-            server.send_message(msg)
-            print("Email poslán!")
+
+load_dotenv()
+SLACK_WEBHOOK_URL = os.getenv("SLACK_WEBHOOK_URL")
+AWS_BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+AWS_REGION = os.getenv("AWS_REGION")
+s3 = boto3.client("s3", region_name=AWS_REGION)
+
+def posli_slack(zprava):
+    payload = {
+        "text": zprava
+    }
+    odpoved = requests.post(SLACK_WEBHOOK_URL, json=payload, timeout=10)
         
-
+    if odpoved.status_code == 200:
+        print("Slack poslán!")
+    else:
+        print(f"Slack chyba: {odpoved.status_code} - {odpoved.text}")
 
 def zkontroluj_web(url):
     cas = datetime.now().strftime("%d.%m.%Y %H:%M:%S")
@@ -39,19 +40,19 @@ def zkontroluj_web(url):
     except:
         with open("monitoring_log.txt", "a") as f:
             f.write(f"{cas} - {url} nefunguje\n")
-        posli_email(f"{url} nefunguje! Čas: {cas}")
+        posli_slack(f"{url} nefunguje! Čas: {cas}")
         return
 
 weby = [
     "https://www.google.com",
     "https://www.facebook.com",
     "https://nexeruno.github.io/hospoda/",
-    "https://www.nefungujeto-123.cz"
+    "https://www.dont-work-123.com"
 ]
 while True:
     for web in weby:
         zkontroluj_web(web)
-        s3.upload_file("monitoring_log.txt", "muj-prvni-bucket-dan-123", "monitoring_log.txt")
+        s3.upload_file("monitoring_log.txt", AWS_BUCKET_NAME, "monitoring_log.txt")
         time.sleep(5)
     
 
